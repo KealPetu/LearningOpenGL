891dbe09e6625d2c183ec66809a549a87d04f15c
KealPetu <kpenafiel99@gmail.com>
2024-12-05 11:38:27 -0500

    metodo para hacer que la ventana no cambie de tamano

--- src/4_dibujandoUnTriangulo/4_traingulo.cpp
+++ src/4_dibujandoUnTriangulo/4_traingulo.cpp
@@ -29,6 +29,7 @@ int main(int argc, char *argv[]){
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);   //La ventana no cambia de tamano
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 
