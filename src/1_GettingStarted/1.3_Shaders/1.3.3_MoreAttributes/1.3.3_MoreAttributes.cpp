b082e1108cf987ff98e6ea36df1effdf11a64086
KealPetu <kpenafiel99@gmail.com>
2025-07-15 18:57:13 -0500

    Remove unused cmath include from MoreAttributes.cpp

--- src/1_GettingStarted/1.3_Shaders/1.3.3_MoreAttributes/1.3.3_MoreAttributes.cpp
+++ src/1_GettingStarted/1.3_Shaders/1.3.3_MoreAttributes/1.3.3_MoreAttributes.cpp
@@ -1,7 +1,6 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-#include <cmath>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
