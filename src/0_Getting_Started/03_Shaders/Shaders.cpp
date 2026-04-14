ef8fe8a2bee928e752fdecf482587b6007f4d4e9
KealPetu <kpenafiel99@gmail.com>
2026-04-13 23:21:07 -0500

    - Cmake build fixed - Shaders.cpp template updated

--- src/0_Getting_Started/03_Shaders/Shaders.cpp
+++ src/0_Getting_Started/03_Shaders/Shaders.cpp
@@ -1,16 +1,23 @@
+//
+// Created by Keal on 4/13/2026.
+//
+
 #include "WindowManager.hpp"
 
+constexpr GLfloat BACKGROUND_COLOR[4]   { 20.4f / 255.f, 20.4f / 255.f, 25.5f / 255.f, 1.f };
+constexpr unsigned int WINDOW_WIDTH	    { 800 };
+constexpr unsigned int WINDOW_HEIGHT	{ 450 };
+
 int main() {
-    initializeGLFW(3, 3);
-    initializeWindow(800, 600, "Shaders");
-
-    while (!windowShouldClose()) {
-        glClearColor(245.f / 255.f, 245.f / 255.f, 245.f / 255.f, 1.f);
+    WindowManager windowManager;
+    WindowManager::initializeGLFW(3, 3);
+    windowManager.initializeWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Polygon");
+    while (!windowManager.windowShouldClose()) {
+        glClearColor(BACKGROUND_COLOR[0], BACKGROUND_COLOR[1], BACKGROUND_COLOR[2], BACKGROUND_COLOR[3]);
         glClear(GL_COLOR_BUFFER_BIT);
-
-        endDrawing();
+        windowManager.endDrawing();
     }
-    destroyWindow();
+    windowManager.destroyWindow();
     glfwTerminate();
     return 0;
 }
