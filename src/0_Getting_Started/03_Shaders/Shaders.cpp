0d1ba4d683990dbc91625ad052c2fc10fe1125dc
keal <kpenafiel99@gmail.com>
2026-04-16 12:51:07 -0500

    - Projects renamed

--- src/0_Getting_Started/03_Shaders/Shaders.cpp
+++ src/0_Getting_Started/03_Shaders/Shaders.cpp
@@ -11,7 +11,7 @@ constexpr unsigned int WINDOW_HEIGHT	{ 450 };
 int main() {
     WindowManager windowManager;
     WindowManager::initializeGLFW(3, 3);
-    windowManager.initializeWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Polygon");
+    windowManager.initializeWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Shaders");
     while (!windowManager.windowShouldClose()) {
         glClearColor(BACKGROUND_COLOR[0], BACKGROUND_COLOR[1], BACKGROUND_COLOR[2], BACKGROUND_COLOR[3]);
         glClear(GL_COLOR_BUFFER_BIT);
