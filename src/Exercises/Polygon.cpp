f51ee7d868bacb3f67a75b1b6ded8d97f86947af
keal <kpenafiel99@gmail.com>
2026-04-16 12:28:55 -0500

    - Polygon.cpp now utilizes EBOs.

--- src/Exercises/Polygon.cpp
+++ src/Exercises/Polygon.cpp
@@ -8,6 +8,7 @@
 
 #include "VAO.hpp"
 #include "VBO.hpp"
+#include "EBO.hpp"
 #include "WindowManager.hpp"
 
 constexpr GLfloat BACKGROUND_COLOR[4]   { 20.4f / 255.f, 20.4f / 255.f, 25.5f / 255.f, 1.f };
@@ -21,6 +22,10 @@ std::vector<GLfloat> generatePolygonVertices(const int nSides, const float size)
 	const float angleIncrement { static_cast<float>(2.0f * M_PI / nSides) };
 	const float normalizedSize { size / MAX_SIZE };
 
+	//Origin vertex (0.0f, 0.0f)
+	vertices.push_back(0.0f);
+	vertices.push_back(0.0f);
+
 	for (int i = 0; i < nSides; ++i) {
 		const float angle = i * angleIncrement;
 		vertices.push_back(normalizedSize * cos(angle)); // x coordinate
@@ -28,6 +33,23 @@ std::vector<GLfloat> generatePolygonVertices(const int nSides, const float size)
 	}
 
 	return vertices;
+}
+
+std::vector<GLuint> generatePolygonIndices(const int nSides) {
+	std::vector<GLuint> indices;
+
+	for (int i = 1; i <= nSides; ++i) {
+		indices.push_back(0);
+		indices.push_back(i);
+
+		if (i == nSides) {
+			indices.push_back(1);
+		} else {
+			indices.push_back(i + 1);
+		}
+	}
+
+	return indices;
 }
 
 int main() {
@@ -109,24 +131,31 @@ int main() {
     float size {};
     std::cin >> size;
 
-	// Vertices
+	// Vertices & indices
 	std::vector vertices {generatePolygonVertices(nSides, size)};
+	std::vector indices {generatePolygonIndices(nSides)};
 
-	//VBO & VAO
+	//VBO, VAO & EBO
 	const VBO vbo {vertices.data(), static_cast<GLsizeiptr>(sizeof(GLfloat) * vertices.size())};
+	const EBO ebo {indices.data(), static_cast<GLsizeiptr>(sizeof(GLuint) * indices.size())};
 	VAO vao {};
 
 	vao.bind();
+	ebo.bind();
 	vao.linkAttrib(vbo, 0, 2, GL_FLOAT, 2 * sizeof(GLfloat), nullptr);
 	VAO::unbind();
+	EBO::unbind();
 
-    while (!windowManager.windowShouldClose()) {
+    // Wireframe mode -----
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	// --------------------
+	while (!windowManager.windowShouldClose()) {
         glClearColor(BACKGROUND_COLOR[0], BACKGROUND_COLOR[1], BACKGROUND_COLOR[2], BACKGROUND_COLOR[3]);
         glClear(GL_COLOR_BUFFER_BIT);
 
     	glUseProgram(shaderProgram);
     	vao.bind();
-    	glDrawArrays(GL_TRIANGLE_FAN, 0, nSides);
+    	glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, nullptr);
         windowManager.endDrawing();
     }
 
